64ca0410ec4d57f76d071b4b3294e258dd12ed2f
keenghost <keenghost@163.com>
2024-09-08 00:02:33 +0800

    feat: add web client

---
+++ src/client-web.ts
@@ -0,0 +1,56 @@
+import { ClientLocalBase, IClientLocalBaseInitOptions } from './class/client-local-base'
+import { ECloseCode, ECloseCodeStr, EWSErrorCode, ISendData, PUBLIC_SEND } from './common/types'
+
+interface IClientWebInitOptions extends IClientLocalBaseInitOptions {}
+
+export class WSWebClient extends ClientLocalBase {
+  #ws: WebSocket
+
+  constructor(inOpts: IClientWebInitOptions) {
+    super(inOpts)
+
+    const protocols: string[] = []
+
+    if (inOpts.token) {
+      protocols.push('__token__' + inOpts.token)
+    }
+
+    this.#ws = new WebSocket(inOpts.url, protocols)
+
+    this.#ws.onopen = () => {
+      this._onOpen()
+    }
+
+    this.#ws.onmessage = inEvt => {
+      const isBinary = inEvt.data instanceof Blob
+
+      this._onMessage(inEvt.data, isBinary)
+    }
+
+    this.#ws.onclose = inEvt => {
+      this._onClose(inEvt.code, inEvt.reason)
+    }
+
+    this.#ws.onerror = () => {
+      this._onError({ code: EWSErrorCode.DEFAULT, error: new Error('websocket error') })
+    }
+  }
+
+  close(inCode?: number, inReason?: string) {
+    const code = inCode === void 0 ? ECloseCode.C1000 : inCode
+    const reason = inReason || ECloseCodeStr[code] || ''
+
+    this.#ws.close(code, reason)
+  }
+
+  [PUBLIC_SEND] = (inData: ISendData) => {
+    return new Promise<void>((resolve, reject) => {
+      if (this.#ws.readyState !== WebSocket.OPEN) {
+        return reject(new Error('websocket is not open'))
+      }
+
+      this.#ws.send(inData)
+      resolve()
+    })
+  }
+}
