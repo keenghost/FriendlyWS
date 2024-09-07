1fe2fd756b57e04dd8f39813031bd02d686a6974
keenghost <keenghost@163.com>
2024-09-08 00:05:23 +0800

    chore: add index.ts

---
+++ index.ts
@@ -0,0 +1,17 @@
+import { WSNodeClient } from './src/client-node'
+import { WSWebClient } from './src/client-web'
+import { WSClientRouter } from './src/plugins/router-client'
+import { WSServerRouter } from './src/plugins/router-server'
+import { WSNodeServer } from './src/server-node'
+
+export * from './src/common/types-export'
+
+export { WSWebClient, WSNodeClient, WSNodeServer, WSClientRouter, WSServerRouter }
+
+export default {
+  WSWebClient,
+  WSNodeClient,
+  WSNodeServer,
+  WSClientRouter,
+  WSServerRouter,
+}
